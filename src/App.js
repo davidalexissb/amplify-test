8a35ce184faa18e90416755e96426af076f913b1
David Alexis <david@umbracorp.cl>
2022-01-12 12:54:45 -0300

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { AmplifySignOut, withAuthenticator } from "@aws-amplify/ui-react";
+import { Auth } from "aws-amplify";
 function App() {
+  Auth.currentUserInfo().then((user) => {
+    console.log(user);
+  });
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <AmplifySignOut />
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
